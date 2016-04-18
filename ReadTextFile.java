bfa56d3a27ae84c25d14955604e201ce2f55a847
Rohan Kulkarni <rohan@Rohans-MacBook-Pro.local>
2016-04-18 03:25:19 -0400

    generated vectors from Songs

--- ReadTextFile.java
+++ ReadTextFile.java
@@ -2,12 +2,88 @@ import java.io.*;
 import java.util.*;
 
 public class ReadTextFile{
-  public static void main(String[] args) {
+
+  public static void main(String[] args) throws IOException{
     String pwd = getCurrentDirectory();
-    String filePath = pwd + "/Songs/love_train.txt";
-    ArrayList allSongs = new ArrayList();
-    allSongs.add(getSongWordList(filePath));
-    System.out.println(allSongs.get(0));
+    ArrayList<ArrayList<String>> allSongs = new ArrayList<ArrayList<String>>();
+
+    try{
+      File path = new File(pwd+"/Songs");
+      File[] files = path.listFiles();
+      for(int i=0;i<files.length;i++){
+        if(files[i].isFile()){
+          String fileName = files[i].getName();
+          String filePath = pwd + "/Songs/" + fileName;
+          allSongs.add(getSongWordList(filePath));
+          System.out.println("One file processed..");
+        }
+      }
+      HashMap<String,Integer> dictionary = new HashMap<String,Integer>();
+      dictionary = generateDictionary(allSongs);
+      int numSongs = allSongs.size();
+      int corpusSize = dictionary.size();
+      int vectorizedMatrix[][] = new int[numSongs][corpusSize];
+      for(int i=0;i<numSongs;i++){
+        ArrayList<String> song = allSongs.get(i);
+        for(String word : song){
+          vectorizedMatrix[i][dictionary.get(word)]++;
+        }
+      }
+      
+      for(int i=0;i<numSongs;i++){
+        for(int j=0;j<corpusSize;j++){
+          System.out.print(vectorizedMatrix[i][j]);
+          System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+    catch(Exception e){
+      System.out.println("Error while adding songs to allSongs:" + e.getMessage());
+    }
+  }
+
+  public static HashMap<String,Integer> generateDictionary(ArrayList<ArrayList<String>> allSongs){
+    HashMap<String,Integer> dictionary = new HashMap<String,Integer>();
+    int index = 0;
+    for(ArrayList<String> song : allSongs){
+      for(String word : song){
+        if(!dictionary.containsKey(word)){
+            dictionary.put(word,index++);
+        }
+      }
+    }
+    return dictionary;
+  }
+
+
+  public static ArrayList<String> eliminateStopwords(String songText,String pwd) throws IOException{
+    String stopWordsFile = pwd + "/Stopwords.txt";
+    ArrayList<String> stopWords = new ArrayList();
+    ArrayList<String> retVal = new ArrayList<>();
+    try{
+      FileReader inputFile = new FileReader(stopWordsFile);
+      BufferedReader bufferReader = new BufferedReader(inputFile);
+      String word;
+      String allStopWords="";
+      while((word = bufferReader.readLine())!=null){
+        word = word.trim();
+        if(word.length() != 0){
+          allStopWords = allStopWords + " " + word;
+        }
+      }
+      String[] stopWordsArray = allStopWords.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+      for(String stopWord : stopWordsArray)
+      {
+        songText = songText.replaceAll(' '+stopWord+' ', "  ");
+      }
+      String[] songArr = songText.split("\\s+");
+      retVal = new ArrayList(Arrays.asList(songArr));
+    }
+    catch(Exception e){
+      System.out.println("Error while reading file line by line:" + e.getMessage());
+    }
+    return retVal;
   }
 
   public static String getCurrentDirectory(){
@@ -15,7 +91,7 @@ public class ReadTextFile{
     return dir;
   }
 
-  public static ArrayList<String> getSongWordList(String filePath){
+  public static ArrayList<String> getSongWordList(String filePath) throws IOException{
     ArrayList<String> retVal = new ArrayList<>();
     try{
       FileReader inputFile = new FileReader(filePath);
@@ -28,14 +104,14 @@ public class ReadTextFile{
           song = song + " " + line;
         }
       }
-      String[] songText = song.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-      retVal = new ArrayList(Arrays.asList(songText));
+      String songText = song.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+      String pwd = getCurrentDirectory();
+      retVal = eliminateStopwords(songText,pwd);
       bufferReader.close();
     }
     catch(Exception e){
       System.out.println("Error while reading file line by line:" + e.getMessage());
     }
-
     return retVal;
   }
 }
